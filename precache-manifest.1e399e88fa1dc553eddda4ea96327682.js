646a092f0903ab481f08880dda99739c019cd8c9
Oscar Islas <oislasreyes@gmail.com>
2020-03-30 21:50:50 -0600

    Subiendo cambios de mi página personal

---
+++ precache-manifest.1e399e88fa1dc553eddda4ea96327682.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116ce8b318362b4a9967fa7c943f6511",
+    "url": "/oscar-islas.github.io/index.html"
+  },
+  {
+    "revision": "5127590f38b17d32e3e8",
+    "url": "/oscar-islas.github.io/static/css/main.93c4a81e.chunk.css"
+  },
+  {
+    "revision": "00c7b729caa53db7d763",
+    "url": "/oscar-islas.github.io/static/js/2.0f34b750.chunk.js"
+  },
+  {
+    "revision": "afcffdf4ebf1780e0970711996cdd7dd",
+    "url": "/oscar-islas.github.io/static/js/2.0f34b750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5127590f38b17d32e3e8",
+    "url": "/oscar-islas.github.io/static/js/main.6c289437.chunk.js"
+  },
+  {
+    "revision": "99144fb2bd04367a2632",
+    "url": "/oscar-islas.github.io/static/js/runtime-main.863bfd89.js"
+  },
+  {
+    "revision": "833bbfcb2c1a840edfa2b04b19c7ce14",
+    "url": "/oscar-islas.github.io/static/media/code.833bbfcb.jpg"
+  }
+]);
